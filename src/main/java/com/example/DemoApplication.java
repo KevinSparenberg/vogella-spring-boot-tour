416926ed615ab4683dfdf17626eef54c10847623
spaab <kevin.sparenberg@lhind.dlh.de>
2018-11-05 16:05:31 +0100

    ^^^^

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,13 +1,16 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
-public class DemoApplication {
+@Controller
+public class HelloWorldController {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    @RequestMapping("/")
+    @ResponseBody
+    String index() {
+        return "Hello, World!";
     }
-}
 
+}
